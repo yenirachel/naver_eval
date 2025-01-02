6170f609b8a5697a055d4f1c6aa2bc3e84d2c81c
Eunbi-Cho <98628614+Eunbi-Cho@users.noreply.github.com>
2025-01-02 13:44:23 +0900

    github pages deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',  // Static HTML 내보내기
+    images: {
+      unoptimized: true,
+    },
+    assetPrefix: '/naver_eval',  // 레포지토리 이름으로 변경
+    basePath: '/naver_eval',     // 레포지토리 이름으로 변경
+  };
+  
+  module.exports = nextConfig;
